7f0795a89f934fef0b90571a0ca6bbb21f568a89
Jakub Kołuda <jko01@outlook.com>
2023-07-14 15:37:27 +0200

    Add querying in database.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,14 +2,10 @@
 
 class Pages extends Controller
 {
-  public function __construct()
-  {
-    
-  }
-
   public function index()
   {
-    $data = ['title' => 'Welcome'];
+    $data = ['title' => 'Welcome',];
+
     $this->view('pages/index', $data);
   }
 
